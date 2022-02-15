262e4fde78649fb2fb1547cb1785dfae6eb92780
abrilquinterog <abril_quintero@hotmail.com>
2022-02-15 12:02:24 -0600

    Se muestran todos lo pokemones

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
- document.getElementById("nombre").innerHTML=`${data.pokemon[0].name}`
- document.getElementById("number").innerHTML=`${data.pokemon[0].num}`
- //document.getElementById("imagen").innerHTML=image.src=`${data.pokemon[0].img}`
-
+ const dataDePokemon = data.pokemon;
+ console.log(dataDePokemon);
+ let allPokemon=document.getElementById("allpokemon")
+ 
+ function mostrarCaracteristicas(item){
+     return ` <section class="card">
+     <img src="${item.img}" alt="" srcset="">
+     <h2>${item.num}</h2>
+     <h3>${item.name}</h3>
+   </section>`
+ }
+console.log("prueba")
+allPokemon.innerHTML=dataDePokemon.map(mostrarCaracteristicas).join(" ");
