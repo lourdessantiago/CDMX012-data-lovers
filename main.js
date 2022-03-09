9c82392acc2401b350821736e401a205d164ac0e
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-03-09 10:00:22 -0600

    Updates

--- main.js
+++ main.js
@@ -34,12 +34,16 @@ function estadicsPoke() {
   const pokeMayoria2 = statsPoke2(dataDePokemon);
   const pokeMinoria = statsPoke3(dataDePokemon);
   const pokeMinoriaSteel = statsPokeSteel(dataDePokemon)
-  allPokemon.innerHTML = `<img src="assets/poketransparentes.png" id=imagenEstats alt=""></img>
+  allPokemon.innerHTML = `<img src="assets/poketransparente.png" id=imagenEstats alt=""></img>
   <div class="cuadradoPorcentajes">
-  <p>Los pokemon de tipo agua representan el : ${pokeMayoria}%<br>
+  <p>Aqui algunos porcentajes de los tipos de Pokemon <br>
+  <br>
+  Los pokemon de tipo agua representan el : ${pokeMayoria}%<br>
   Los pokemon de tipo hierba representan el: ${pokeMayoria2}%<br>
   Los pokemon de tipo dragon representan el :${pokeMinoria}%<br>
-  Los pokemon de tipo acero representan el : ${pokeMinoriaSteel}%</p> 
+  Los pokemon de tipo acero representan el : ${pokeMinoriaSteel}%<br> 
+  <br>
+  En la grafica podrás observar cuantos Pokemon de cada tipo. <br> </p> 
   <canvas id="myChart" width="400" height="400"></canvas>`;
   estadisticasGrafica(dataDePokemon, 'water');
   const data = {
